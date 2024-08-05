c38a1b1a3650e63f7b6f5f56b1c2a6ce59878f68
alensiva <alen8261788@gmail.com>
2024-08-05 19:58:02 +0530

    Add files via upload

---
+++ patterns/invertedtri.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, rows;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = rows; i >= 1; i--) {
+        for (j = rows; j > i; j--) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
